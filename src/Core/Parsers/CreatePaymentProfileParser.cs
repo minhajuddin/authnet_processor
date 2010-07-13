df86f8c6d990239a3bb6f12630557e93380538b6
rafi <rafi@cosmicvent.com>
2010-07-13 18:40:21 +0530

    parser to the create payment profile is done

---
+++ src/Core/Parsers/CreatePaymentProfileParser.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Authnet.Parsers {
+    public class CreatePaymentProfileParser : IParser {
+        public Response Parse(string rawXml) {
+            var response = new Response();
+            var set = new ParameterSet();
+
+            var doc = XDocument.Parse(rawXml);
+            XNamespace schema = "AnetApi/xml/v1/schema/AnetApiSchema.xsd";
+            var root = doc.Descendants(schema + "createCustomerPaymentProfileResponse");
+            response.ResultCode = root.Descendants(schema + "resultCode").First().Value;
+            response.Message = root.Descendants(schema + "text").First().Value;
+            set["customerPaymentProfileId"] = root.Descendants(schema + "customerPaymentProfileId").First().Value;
+            set["validationDirectResponse"] = root.Descendants(schema + "validationDirectResponse").First().Value;
+
+            response.ParameterSet = set;
+            return response;
+        }
+    }
+}
---
+++ src/Tests/Unit/Parsers/CreatePaymentProfileParserTests.cs
@@ -0,0 +1,32 @@
+using System;
+using NUnit.Framework;
+
+namespace Tests.Unit.Parsers {
+    [TestFixture]
+    public class CreatePaymentProfileParserTests {
+        [Test]
+        public void CanParse() {
+            var rawXml = @"<?xml version='1.0' encoding='utf-8'?>
+                            <createCustomerPaymentProfileResponse xmlns='AnetApi/xml/v1/schema/AnetApiSchema.xsd'>
+                                <messages>
+                                    <resultCode>Ok</resultCode>
+                                    <message>
+                                        <code>I00001</code>
+                                        <text>Successful.</text>
+                                    </message>
+                                </messages>
+                                <customerPaymentProfileId>20000</customerPaymentProfileId>
+                                <validationDirectResponse>1,1,1,This transaction has been approved.,000000,Y,2000000000,none,Test transaction for ValidateCustomerPaymentProfile.,0.01,CC,auth_only,custId123,John,Doe,,123 Main St.,Bellevue,WA,98004,USA,000-000-0000,,mark@example.com,,,,,,,,,0.00,0.00,0.00,,none,D18EB6B211FE0BBF556B271FDA6F92EE,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,</validationDirectResponse>
+                            </createCustomerPaymentProfileResponse>";
+
+            var parser = new CreatePaymentProfileParser();
+            var result = parser.Parse(rawXml);
+
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual("Successful.", result.Message);
+            Assert.AreEqual("20000", result.ParameterSet["customerPaymentProfileId"].ToString());
+            Assert.AreEqual("1,1,1,This transaction has been approved.,000000,Y,2000000000,none,Test transaction for ValidateCustomerPaymentProfile.,0.01,CC,auth_only,custId123,John,Doe,,123 Main St.,Bellevue,WA,98004,USA,000-000-0000,,mark@example.com,,,,,,,,,0.00,0.00,0.00,,none,D18EB6B211FE0BBF556B271FDA6F92EE,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,", result.ParameterSet["validationDirectResponse"].ToString());
+
+        }
+    }
+}
