3db5b2893c1553863947da7e38856cffa364524e
rafi <rafi@cosmicvent.com>
2010-07-14 13:12:07 +0530

    Create Customer Profile Transaction Parser

---
+++ src/Core/Parsers/CreateCustomerProfileTransactionParser.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using System.Xml.Linq;
+using Authnet;
+using Authnet.Parsers;
+
+namespace Tests.Unit.Parsers {
+    public class CreateCustomerProfileTransactionParser : IParser {
+        public Response Parse(string rawXml) {
+            var response = new Response();
+            var set = new ParameterSet();
+
+            var doc = XDocument.Parse(rawXml);
+            XNamespace schema = "AnetApi/xml/v1/schema/AnetApiSchema.xsd";
+            var root = doc.Descendants(schema + "createCustomerProfileTransactionResponse");
+            response.ResultCode = root.Descendants(schema + "resultCode").First().Value;
+            response.Message = root.Descendants(schema + "text").First().Value;
+            set["directResponse"] = root.Descendants(schema + "directResponse").First().Value;
+
+            response.ParameterSet = set;
+            return response;
+        }
+    }
+}
---
+++ src/Tests/Unit/Parsers/CreateCustomerProfileTransactionParserTests.cs
@@ -0,0 +1,33 @@
+using System;
+using NUnit.Framework;
+
+namespace Tests.Unit.Parsers {
+    [TestFixture]
+    public class CreateCustomerProfileTransactionParserTests {
+        [Test]
+
+        public void CanParse() {
+
+            var parser = new CreateCustomerProfileTransactionParser();
+
+            var rawXml = @"<?xml version='1.0' encoding='utf-8'?>
+<createCustomerProfileTransactionResponse xmlns='AnetApi/xml/v1/schema/AnetApiSchema.xsd'>
+  <messages>
+    <resultCode>Ok</resultCode>
+    <message>
+      <code>I00001</code>
+      <text>Successful.</text>
+    </message>
+  </messages>
+  <directResponse>1,1,1,This transaction has been approved.,000000,Y,2000000001,INV000001,description of transaction,10.95,CC,auth_capture,custId123,John,Doe,,123 Main St.,Bellevue,WA,98004,USA,000-000-0000,,mark@example.com,John,Doe,,123 Main St.,Bellevue,WA,98004,USA,1.00,0.00,2.00,FALSE,PONUM000001,D18EB6B211FE0BBF556B271FDA6F92EE,M,2,,,,,,,,,,,,,,,,,,,,,,,,,,,,</directResponse>
+</createCustomerProfileTransactionResponse>";
+
+            var response = parser.Parse(rawXml);
+
+            Assert.AreEqual(true, response.Success);
+            Assert.AreEqual("Successful.", response.Message);
+            Assert.IsNotNullOrEmpty(response.ParameterSet["directResponse"].ToString());
+
+        }
+    }
+}
