9bb791fba71d6838dcfb1a842bb6e0856d68a4c2
rafi <rafi@cosmicvent.com>
2010-07-16 19:34:01 +0530

    deleteCustomerPaymentProfile template done

---
+++ src/Tests/Integration/Templating/DeleteCustomerPaymentProfileTemplateViewTests.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using Authnet;
+using NUnit.Framework;
+
+namespace Tests.Integration.Templating {
+    [TestFixture]
+    public class DeleteCustomerPaymentProfileTemplateViewTests {
+
+        private Dictionary<string, object> _getPaymentProfileAttributes;
+        private IProfileAttributes _profileAttributes;
+        private IPaymentProfileAttributes _paymentProfileAttributes;
+
+        [SetUp]
+        public void setup() {
+
+            _profileAttributes = ObjectMother.GetMockProfileAttributes(x =>
+            {
+                x.GateWayId = "123215";
+            });
+
+            _paymentProfileAttributes = ObjectMother.GetMockPaymentProfileAttributes(x =>
+            {
+                x.GateWayId = "829831";
+            });
+
+
+            _getPaymentProfileAttributes = new Dictionary<string, object>();
+            _getPaymentProfileAttributes.Add("profileAttributes", _profileAttributes);
+            _getPaymentProfileAttributes.Add("paymentProfileAttributes", _paymentProfileAttributes);
+
+
+        }
+
+        [Test]
+        public void RenderDeleteCustomerPaymentProfileTemplateView() {
+            var factory = TestHelper.TemplateFactory;
+            var template = factory.GetInstance("deleteCustomerPaymentProfileRequest.spark");
+            template.Authentication = ObjectMother.TestAuthentication;
+            var result = template.Render(_getPaymentProfileAttributes);
+
+            var expectedXml = @"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<deleteCustomerPaymentProfileRequest xmlns=""AnetApi/xml/v1/schema/AnetApiSchema.xsd"">  <merchantAuthentication>
+    <name>54PB5egZ</name>
+    <transactionKey>48V258vr55AE8tcg</transactionKey>
+  </merchantAuthentication>
+  <customerProfileId>123215</customerProfileId>
+  <customerPaymentProfileId>829831</customerPaymentProfileId>
+</deleteCustomerPaymentProfileRequest>";
+            Assert.AreEqual(expectedXml, result);
+
+        }
+
+    }
+}
